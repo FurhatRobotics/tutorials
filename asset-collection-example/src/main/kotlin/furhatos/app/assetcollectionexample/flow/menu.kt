a703cc52b8ed22a6517fd1291b3d6b555d073d76
Mathieu <mathieu@furhatrobotics.com>
2023-02-10 11:30:49 +0100

    Official doc and final changes

--- asset-collection-example/src/main/kotlin/furhatos/app/assetcollectionexample/flow/menu.kt
+++ asset-collection-example/src/main/kotlin/furhatos/app/assetcollectionexample/flow/menu.kt
@@ -5,6 +5,7 @@ import furhatos.app.assetcollectionexample.flow.gestureslibrary.gesturesLibExamp
 import furhatos.app.assetcollectionexample.flow.nluLibrary.nluLibExample
 import furhatos.app.assetcollectionexample.flow.usersLibrary.usersLibExample
 import furhatos.app.assetcollectionexample.flow.utilsLibrary.utilsLibExample
+import furhatos.app.assetcollectionexample.logger
 import furhatos.flow.kotlin.Color
 import furhatos.flow.kotlin.State
 import furhatos.flow.kotlin.furhat
@@ -36,12 +37,14 @@ val Menu: State = state {
         goto(utilsLibExample)
     }
 
-    onButton("About this skill") {
+    onButton("About this skill", color = Color.Green) {
         furhat.say("This skill demonstrates the different features and functions that are available in the Asset Collection Library, " +
                 "that is one part of the Furhat Library. Read more about the Furhat Library in our docs page at docs at furhat dot i o. " +
                 "In short, the asset collection is a place where we add experimental features and useful functions that have not yet " +
                 "been included in directly in the Furhat platform. You can get access to these features directly from intelliJ. " +
-                "In the future we hope to also include features and functions created by people from the Furhat Community. ")
+                "In the future we hope to also include features and functions created by people from the Furhat Community.")
+        logger.info("Official StandardLibraryCollection documentation available here : https://furhat-files.s3.eu-west-1.amazonaws.com/standardlibrary-docs/1.2.0/index.html")
+        furhat.say("I printed the link to the library documentation in the console log.")
     }
 }
 
--- asset-collection-example/src/main/kotlin/furhatos/app/assetcollectionexample/init.kt
+++ asset-collection-example/src/main/kotlin/furhatos/app/assetcollectionexample/init.kt
@@ -19,9 +19,7 @@ val Init = state {
         if (users.count >= 1) {
             furhat.attend(users.random)
         }
-        //TODO : include official link !
-        logger.info("Official StandardLibraryCollection documentation available here : " +
-                "https://furhat-files.s3.eu-west-1.amazonaws.com/standardlibrary-docs/1.2.0-review-SNAP/-standard-library-collection/furhat.libraries.standard.utils/-google-sheets-integration/index.html")
+        logger.info("Official StandardLibraryCollection documentation available here : https://furhat-files.s3.eu-west-1.amazonaws.com/standardlibrary-docs/1.2.0/index.html")
         furhat.say("Hello ! This skill is a demonstration of what functions are available in the Asset Collection Library and how these functions can be used.")
         furhat.say("There are five different libraries available. Please wizard your way through the skill to explore them.")
 
